dc48ce561b3ad6de5931199408269b474839c431
Yangyu Chen <cyy@cyyself.name>
2024-06-19 17:56:43 +0000

    core_imp_def: add patch gen

--- core_imp_def.py
+++ core_imp_def.py
@@ -24,6 +24,19 @@ def gen_core_imp_def_dict(pl, desc=dict()):
             })
     return res
 
+def gen_core_json_patch(filename, pl, desc):
+    res = gen_core_imp_def_dict(pl, desc)
+    import json
+    lines = json.dumps(res, indent=4).split("\n")
+    return f"""
+diff --git a/{filename} b/{filename}
+new file mode 100644
+--- /dev/null
++++ b/{filename}
+@@ -0,0 +1,{len(lines)} @@
+{"\n".join(map(lambda x: f"+{x}", lines))}
+""".strip()
+
 if __name__ == "__main__":
     import sys
     if len(sys.argv) < 2:
