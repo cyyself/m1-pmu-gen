30913b54ed33bf38ca5a3116db30824244f3aa31
Yangyu Chen <cyy@cyyself.name>
2025-10-29 23:15:15 +0800

    apple_pdf_extract.py: extract table to support latest apple pdf

--- apple_pdf_extract.py
+++ apple_pdf_extract.py
@@ -8,35 +8,43 @@
 def extract_event_desc(pdf_path):
     import pymupdf
     import re
-    end_sentense = ["Apple Silicon CPU Optimization Guide", "spacer"]
+    import tqdm
+    import sys
     def is_event_name(s):
         s = s.strip()
-        return re.match(r"^[A-Z0-9_]+$", s)
+        return re.match(r"^[A-Z][A-Z0-9_]+$", s)
     doc = pymupdf.open(pdf_path)
     result = dict()
-    for page in doc:
-        text = page.get_text().split("\n")
-        for idx in range(len(text)-1):
-            if [text[idx].strip(), text[idx+1].strip()] == \
-               ["Event Name", "Brief Description"]:
-                    # Here we start to extract event description
-                    idx += 2
-                    while idx + 1 < len(text):
-                        event = text[idx].strip()
-                        if not is_event_name(event):
-                            break
-                        idx += 1
-                        desc = text[idx].strip()
-                        idx += 1
-                        desc_idx = idx
-                        while desc_idx < len(text) and \
-                              (not is_event_name(text[desc_idx])) and \
-                              (not text[desc_idx].strip() in end_sentense):
-                            desc += " " + text[desc_idx].strip()
-                            desc_idx += 1
-                        result[event] = desc
-                        idx = desc_idx
-                    break
+    for page in tqdm.tqdm(doc.pages(), total=doc.page_count):
+        text = page.get_text()
+        if "Event Name" not in text or \
+           "Brief Description" not in text:
+            continue
+        tabs = page.find_tables()
+        if tabs.tables:
+            for tab in tabs.tables:
+                cur_table = tab.extract()
+                try:
+                    header = cur_table[0]
+                    if header[0].strip() == 'Event Name' and \
+                       header[1].strip() == 'Brief Description':
+                        # Here we start to extract event description
+                        for row in cur_table[1:]:
+                            event = ""
+                            desc = ""
+                            try:
+                                event = row[0].strip()
+                                desc = row[1].strip()
+                            except:
+                                continue
+                            if "\n" in event:
+                                event = event.split("\n")[0].strip()
+                            if not is_event_name(event):
+                                continue
+                            result[event] = " ".join([x.strip() for x in desc.split("\n")])
+                except:
+                    print("Failed to extract table on page:", page.number, file=sys.stderr)
+                    exit(1)
     return result
 
 if __name__ == "__main__":
